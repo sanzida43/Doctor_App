2fe78f8605f88716b4f81ca692b0203492a08cbb
sanzida_mou <127539782+sanzida43@users.noreply.github.com>
2023-03-11 00:01:51 +0600

    model class

---
+++ doctorDetail.dart
@@ -0,0 +1,49 @@
+class Doctor {
+  String? profileImg;
+  String? name;
+  String? subtitle;
+  String? price;
+  String? year;
+  String? like;
+
+  Doctor({this.profileImg, this.name, this.subtitle, this.price, this.year, this.like});
+}
+
+List<Doctor> doctorItems = [
+  Doctor(
+      profileImg: "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZG9jdG9yfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      name: "Darrel Steward",
+      subtitle: "General Practitioner",
+      year: "3",
+      like: "92",
+      price: "80"),
+  Doctor(
+      profileImg: "https://images.theconversation.com/files/304957/original/file-20191203-66986-im7o5.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
+      name: "Wade Warren ",
+      subtitle: "General Practitioner",
+      year: "3",
+      like: "92",
+      price: "80"),
+  Doctor(
+      profileImg: "https://media.istockphoto.com/id/1189304032/photo/doctor-holding-digital-tablet-at-meeting-room.jpg?s=612x612&w=0&k=20&c=RtQn8w_vhzGYbflSa1B5ea9Ji70O8wHpSgGBSh0anUg=",
+      name: "Wade Warren ",
+      subtitle: "General Practitioner",
+      year: "3",
+      like: "92",
+      price: "80"),
+  Doctor(
+      profileImg: "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZG9jdG9yfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      name: "Darrel Steward",
+      subtitle: "General Practitioner",
+      year: "3",
+      like: "92",
+      price: "80"),
+  Doctor(
+      profileImg: "https://media.istockphoto.com/id/1189304032/photo/doctor-holding-digital-tablet-at-meeting-room.jpg?s=612x612&w=0&k=20&c=RtQn8w_vhzGYbflSa1B5ea9Ji70O8wHpSgGBSh0anUg=",
+      name: "Wade Warren ",
+      subtitle: "General Practitioner",
+      year: "3",
+      like: "92",
+      price: "80"),
+
+];
